a143328594be6b19a1a06218f51bf485e2055e5d
Johannes <johannes.grill@ijg.at>
2023-07-11 09:52:20 +0000

    Energie Steiermark +1,2ct/kWh statt aWATTar +3%

--- frontend/src/modules/useElectricityPrices.ts
+++ frontend/src/modules/useElectricityPrices.ts
@@ -41,10 +41,12 @@ export default function useElectricityPrices() {
       return 0
     }
     // awattar takes 3 % fee in either direction
-    if (usedPrice.marketprice < 0) {
-      return Math.floor(usedPrice.marketprice * 1000 * 0.97)
-    }
-    return Math.floor(usedPrice.marketprice * 1000 * 1.03)
+    // if (usedPrice.marketprice < 0) {
+    //  return Math.floor(usedPrice.marketprice * 1000 * 0.97)
+    // }
+    // return Math.floor(usedPrice.marketprice * 1000 * 1.03)
+    // Energie Steiermark adds 1,2 ct/kWh on top of EPEX price
+    return usedPrice.marketprice * 1000 + 1.2)
   }
 
   return { loadForDateIso, loading, loadingFailed, prices, priceForDate }
