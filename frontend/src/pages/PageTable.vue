0c857da191d734577207e5ee47e195810acad9df
Thomas Schagerl <tom@satoshiengineering.com>
2024-10-17 13:43:33 +0200

    [REFACTOR] PageTable optimize layout

--- frontend/src/pages/PageTable.vue
+++ frontend/src/pages/PageTable.vue
@@ -1,6 +1,89 @@
 <template>
   <div class="flex-1 flex flex-col items-center overflow-hidden">
-    <div class="relative w-full flex mt-4 px-2 justify-left md:justify-center">
+    <div v-if="showLoadingAnimation" class="flex-1 grid justify-center content-center">
+      <AnimatedLoadingWheel />
+    </div>
+    <div
+      v-else-if="showContent && currentDateIso != null && loadingFailed.includes(currentDateIso)"
+      class="flex-1 grid justify-center content-center text-red-600"
+    >
+      {{ $t('errors.loadingPricesFailed') }}
+    </div>
+    <div
+      v-else-if="showContent"
+      class="w-full max-w-lg mx-auto pt-1 pr-2 grid grid-cols-[40px_1fr_1fr_1fr] gap-0.5 text-xs"
+    >
+      <div />
+      <div class="py-0.5 px-3 text-center font-bold">
+        {{ currentDate.minus({ days: 1 }).toFormat('dd. LL.') }}
+      </div>
+      <div class="py-0.5 px-3 text-center font-bold">
+        {{ currentDate.toFormat('dd. LL.') }}
+      </div>
+      <div class="py-0.5 px-3 text-center font-bold">
+        {{ currentDate.plus({ days: 1 }).toFormat('dd. LL.') }}
+      </div>
+      <template
+        v-for="(price, index) in prices"
+        :key="index"
+      >
+        <div class="py-0.5 px-3 text-right">
+          {{ String(price.hour).padStart(2, '0') }}
+        </div>
+        <div
+          class="py-0.5 px-3 rounded-md text-center"
+          :class="{
+            'bg-green-500': price.pricePrev <= 0,
+            'bg-green-200': price.pricePrev > 0 && price.pricePrev <= 5,
+            'bg-yellow-200': price.pricePrev > 5 && price.pricePrev <= 10,
+            'bg-red-200': price.pricePrev > 10 && price.pricePrev <= 15,
+            'bg-red-500': price.pricePrev > 15 && price.pricePrev <= 25,
+            'bg-red-800': price.pricePrev > 25,
+            'border-4': currentDate.minus({ days: 1 }).toISODate() === DateTime.now().toISODate()
+              && DateTime.now().toFormat('H') === String(price.hour),
+            'font-bold': currentDate.minus({ days: 1 }).toISODate() === DateTime.now().toISODate()
+              && DateTime.now().toFormat('H') === String(price.hour),
+          }"
+        >
+          {{ price.pricePrev.toFixed(2) }}
+        </div>
+        <div
+          class="py-0.5 px-3 rounded-md text-center"
+          :class="{
+            'bg-green-500': price.price <= 0,
+            'bg-green-200': price.price > 0 && price.price <= 5,
+            'bg-yellow-200': price.price > 5 && price.price <= 10,
+            'bg-red-200': price.price > 10 && price.price <= 15,
+            'bg-red-500': price.price > 15 && price.price <= 25,
+            'bg-red-800': price.price > 25,
+            'border font-bold': currentDate.toISODate() === DateTime.now().toISODate()
+              && DateTime.now().toFormat('H') === String(price.hour),
+          }"
+        >
+          {{ price.price.toFixed(2) }}
+        </div>
+        <div
+          v-if="currentDate.plus({ days: 1 }) <= maxDate"
+          class="py-0.5 px-3 rounded-md text-center"
+          :class="{
+            'bg-green-500': price.priceNext <= 0,
+            'bg-green-200': price.priceNext > 0 && price.priceNext <= 5,
+            'bg-yellow-200': price.priceNext > 5 && price.priceNext <= 10,
+            'bg-red-200': price.priceNext > 10 && price.priceNext <= 15,
+            'bg-red-500': price.priceNext > 15 && price.priceNext <= 25,
+            'bg-red-800': price.priceNext > 25,
+            'border-4': currentDate.plus({ days: 1 }).toISODate() === DateTime.now().toISODate()
+              && DateTime.now().toFormat('H') === String(price.hour),
+            'font-bold': currentDate.plus({ days: 1 }).toISODate() === DateTime.now().toISODate()
+              && DateTime.now().toFormat('H') === String(price.hour),
+          }"
+        >
+          {{ price.priceNext.toFixed(2) }}
+        </div>
+        <div v-else />
+      </template>
+    </div>
+    <div class="relative w-full flex my-4 px-2 justify-center">
       <button
         class="bg-gray-300 hover:enabled:bg-gray-400 text-gray-800 mx-2 py-2 px-4 rounded-l disabled:opacity-50"
         :disabled="!prevDateValid || (currentDateIso != null && loadingPrices.includes(currentDateIso))"
@@ -25,98 +108,6 @@
       >
         {{ type === 'xs' ? '>' : $t('components.datepicker.next') }}
       </button>
-    </div>
-    <div v-if="showLoadingAnimation" class="flex-1 grid justify-center content-center">
-      <AnimatedLoadingWheel />
-    </div>
-    <div
-      v-else-if="showContent && currentDateIso != null && loadingFailed.includes(currentDateIso)"
-      class="flex-1 grid justify-center content-center text-red-600"
-    >
-      {{ $t('errors.loadingPricesFailed') }}
-    </div>
-    <div v-else-if="showContent" class="w-full overflow-x-auto">
-      <table class="mt-10 mx-auto">
-        <tr>
-          <th />
-          <th class="p-4">
-            <td>{{ currentDate.minus({ days: 1 }).toFormat('dd. LL.') }}</td>
-          </th>
-          <th class="p-4">
-            <td>{{ currentDate.toFormat('dd. LL.') }}</td>
-          </th>
-          <th class="p-4">
-            <td>{{ currentDate.plus({ days: 1 }).toFormat('dd. LL.') }}</td>
-          </th>
-        </tr>
-        <tr
-          v-for="(price, index) in prices"
-          :key="index"
-        >
-          <td class="p-1">
-            {{ String(price.hour).padStart(2, '0') }}
-          </td>
-          <td class="text-center">
-            <span
-              class="block m-1 py-1 px-3 rounded-md"
-              :class="{
-                'bg-green-500': price.pricePrev <= 0,
-                'bg-green-200': price.pricePrev > 0 && price.pricePrev <= 5,
-                'bg-yellow-200': price.pricePrev > 5 && price.pricePrev <= 10,
-                'bg-red-200': price.pricePrev > 10 && price.pricePrev <= 15,
-                'bg-red-500': price.pricePrev > 15 && price.pricePrev <= 25,
-                'bg-red-800': price.pricePrev > 25,
-                'border-4': currentDate.minus({ days: 1 }).toISODate() === DateTime.now().toISODate()
-                  && DateTime.now().toFormat('H') === String(price.hour),
-                'font-bold': currentDate.minus({ days: 1 }).toISODate() === DateTime.now().toISODate()
-                  && DateTime.now().toFormat('H') === String(price.hour),
-              }"
-            >
-              {{ price.pricePrev.toFixed(2) }}
-            </span>
-          </td>
-          <td class="text-center">
-            <span
-              class="block m-1 py-1 px-3 rounded-md"
-              :class="{
-                'bg-green-500': price.price <= 0,
-                'bg-green-200': price.price > 0 && price.price <= 5,
-                'bg-yellow-200': price.price > 5 && price.price <= 10,
-                'bg-red-200': price.price > 10 && price.price <= 15,
-                'bg-red-500': price.price > 15 && price.price <= 25,
-                'bg-red-800': price.price > 25,
-                'border-4': currentDate.toISODate() === DateTime.now().toISODate()
-                  && DateTime.now().toFormat('H') === String(price.hour),
-                'font-bold': currentDate.toISODate() === DateTime.now().toISODate()
-                  && DateTime.now().toFormat('H') === String(price.hour),
-              }"
-            >
-              {{ price.price.toFixed(2) }}
-            </span>
-          </td>
-          <td class="text-center">
-            <template v-if="currentDate.plus({ days: 1 }) <= maxDate">
-              <span
-                class="block m-1 py-1 px-3 rounded-md"
-                :class="{
-                  'bg-green-500': price.priceNext <= 0,
-                  'bg-green-200': price.priceNext > 0 && price.priceNext <= 5,
-                  'bg-yellow-200': price.priceNext > 5 && price.priceNext <= 10,
-                  'bg-red-200': price.priceNext > 10 && price.priceNext <= 15,
-                  'bg-red-500': price.priceNext > 15 && price.priceNext <= 25,
-                  'bg-red-800': price.priceNext > 25,
-                  'border-4': currentDate.plus({ days: 1 }).toISODate() === DateTime.now().toISODate()
-                    && DateTime.now().toFormat('H') === String(price.hour),
-                  'font-bold': currentDate.plus({ days: 1 }).toISODate() === DateTime.now().toISODate()
-                    && DateTime.now().toFormat('H') === String(price.hour),
-                }"
-              >
-                {{ price.priceNext.toFixed(2) }}
-              </span>
-            </template>
-          </td>
-        </tr>
-      </table>
     </div>
   </div>
 </template>
